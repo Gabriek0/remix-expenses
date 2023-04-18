e824787b073db980811bea0e016e83659306b3df
Gabriel Henrique <gabriel.sanches170@gmail.com>
2023-04-18 20:30:54 -0300

    :hammer: feat: Create ExpenseStatistics component

--- app/components/Expenses/index.ts
+++ app/components/Expenses/index.ts
@@ -3,3 +3,4 @@ export { ChartBar } from "./ChartBar";
 export { ExpenseForm } from "./ExpenseForm";
 export { ExpensesList } from "./ExpenseList";
 export { ExpenseListItem } from "./ExpenseListItem";
+export { ExpenseStatistics } from "./ExpenseStatistics";
