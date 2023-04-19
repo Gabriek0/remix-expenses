9b8f658c278e6397d2e5270dcf8502515a2881e5
Gabriel Henrique <gabriel.sanches170@gmail.com>
2023-04-19 00:05:37 -0300

    :hammer: feat: Add functionality to close modal with esc key and click outside modal

--- app/components/Util/Modal/index.tsx
+++ app/components/Util/Modal/index.tsx
@@ -1,16 +1,30 @@
 import { useNavigate } from "@remix-run/react";
-import { ReactNode } from "react";
+import React, { ReactNode, useCallback, useEffect } from "react";
 
 interface Props {
   children: ReactNode;
-  // onClose: () => void;
 }
+
+const KEY_NAME_ESC = "Escape";
 
 export function Modal({ children }: Props) {
   const navigate = useNavigate();
 
+  // close modal with 'esc' key
+  useEffect(() => {
+    const useEscapeHandler = (event: KeyboardEvent) => {
+      if (event.key === KEY_NAME_ESC) {
+        navigate("..");
+      }
+    };
+
+    document.addEventListener("keydown", useEscapeHandler);
+
+    return () => document.removeEventListener("keydown", useEscapeHandler);
+  }, []);
+
   return (
-    <div className="modal-backdrop" onClick={() => navigate("/expenses")}>
+    <div className="modal-backdrop" onClick={() => navigate("..")}>
       <dialog
         open
         className="modal"
