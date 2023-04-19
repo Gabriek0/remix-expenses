d6a192a6d6d53cec3ff0d6c9b138c8741ef24dc3
Gabriel Henrique <gabriel.sanches170@gmail.com>
2023-04-18 22:44:35 -0300

    :hammer: feat: Add content to home page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,7 @@
-import type { V2_MetaFunction } from "@remix-run/react";
+import { Link, V2_MetaFunction } from "@remix-run/react";
+
+// Icons
+import { FaArrowRight, FaChartBar, FaDollarSign } from "react-icons/fa";
 
 export const meta: V2_MetaFunction = () => {
   return [{ title: "Home" }];
@@ -6,8 +9,45 @@ export const meta: V2_MetaFunction = () => {
 
 export default function Index() {
   return (
-    <>
-      <h1>Home</h1>
-    </>
+    <main>
+      <section className="marketing-section">
+        <header>
+          <FaDollarSign />
+          <h2>A Central Space</h2>
+        </header>
+        <div className="marketing-content">
+          <div className="marketing-image">
+            <img
+              src="images/expenses-management.jpg"
+              alt="A list of expenses."
+            />
+          </div>
+          <div className="marketing-explanation">
+            <p>Manage your expenses in one central place.</p>
+            <p>
+              <Link className="cta" to="/expenses">
+                <span>Get Started</span>
+                <FaArrowRight />
+              </Link>
+            </p>
+          </div>
+        </div>
+      </section>
+      <section className="marketing-section">
+        <header>
+          <FaChartBar />
+          <h2>Detailed Analytics</h2>
+        </header>
+        <div className="marketing-content">
+          <p className="marketing-explanation">
+            Benefit from best-in-class analytics to understand your spending
+            patterns.
+          </p>
+          <div className="marketing-image">
+            <img src="images/expenses-chart.jpg" alt="A demo bar chart." />
+          </div>
+        </div>
+      </section>
+    </main>
   );
 }
