dcd53232ff7fcb249374e77d56fdd3d05f7cf4b4
Gabriel Henrique <gabriel.sanches170@gmail.com>
2023-05-30 20:42:01 -0300

    :hammer: feat: Add check for existing users and emails

---
+++ app/features/auth/auth.server.ts
@@ -0,0 +1,18 @@
+import { db } from "~/utils/db.server";
+import { Credentials } from "~/utils/validations/validation.server";
+
+export async function signup({ email, password }: Credentials) {
+  const existingUser = await db.user.findFirst({
+    where: {
+      email,
+    },
+  });
+
+  if (existingUser) {
+    const error = new Error(
+      "A user with the provided email address exists already."
+    );
+
+    throw error;
+  }
+}
