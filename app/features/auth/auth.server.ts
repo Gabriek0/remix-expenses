8822452ed77d3f78dbb170974eff5ebaaab692db
Gabriel Henrique <gabriel.sanches170@gmail.com>
2023-05-30 20:43:02 -0300

    :hammer: feat: Add console.log to password in signup function server-side to use this variable

--- app/features/auth/auth.server.ts
+++ app/features/auth/auth.server.ts
@@ -2,6 +2,8 @@ import { db } from "~/utils/db.server";
 import { Credentials } from "~/utils/validations/validation.server";
 
 export async function signup({ email, password }: Credentials) {
+  console.log(password);
+
   const existingUser = await db.user.findFirst({
     where: {
       email,
